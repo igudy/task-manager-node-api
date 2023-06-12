9d15e7cdacf2d64f26d2b1ce084d928955be6b3b
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-06-12 15:44:14 +0100

    Done with delete functionality

--- app.js
+++ app.js
@@ -1,20 +1,57 @@
-require("./db/connect")
+// require("./db/connect")
+// const express = require("express")
+// const app = express()
+// const connectDB = require("./db/connect")
+// require("dotenv").config()
+// const cors = require("cors")
+
+// // middleware
+// app.use(cors())
+// // app.use(express.static("./public"))
+// app.use(express.json())
+
+// // routes
+// const tasks = require("./routes/tasks")
+
+// // get-to be shown on the webpage
+// // app.get("/hello", (req, res) => {
+// //   res.send("Task Manager")
+// // })
+
+// // Tasks route
+// app.use("/api/v1/tasks", tasks)
+
+// // Assigning port value
+// const port = 3000
+
+// // Try and catch for listening to server
+// const start = async () => {
+//   try {
+//     await connectDB(process.env.MONGO_URI)
+//     app.listen(port, console.log(`Server is listening on port ${port}...`))
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// start()
+
 const express = require("express")
 const app = express()
 const connectDB = require("./db/connect")
 require("dotenv").config()
+const cors = require("cors")
 
 // middleware
-app.use(express.static("./public"))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+)
 app.use(express.json())
 
 // routes
 const tasks = require("./routes/tasks")
-
-// get-to be shown on the webpage
-// app.get("/hello", (req, res) => {
-//   res.send("Task Manager")
-// })
 
 // Tasks route
 app.use("/api/v1/tasks", tasks)
